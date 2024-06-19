5129244619a5782ac5c8a24f6075fee028884595
george <1014175823@qq.com>
2024-06-19 18:27:28 +0800

    Initial Commit

---
+++ DynamicTextField/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DynamicTextField
+//
+//  Created by 王庭志 on 2024/6/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DynamicTextField/DynamicTextFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicTextFieldApp.swift
+//  DynamicTextField
+//
+//  Created by 王庭志 on 2024/6/19.
+//
+
+import SwiftUI
+
+@main
+struct DynamicTextFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
